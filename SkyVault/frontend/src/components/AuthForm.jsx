abbe3e5f649a369dc4b5c4abe9f6fff19006d6e3
RoziznanVadim <roziznan_v@mail.ru>
2025-01-21 11:14:53 +0300

    Add component Loding

--- SkyVault/frontend/src/components/AuthForm.jsx
+++ SkyVault/frontend/src/components/AuthForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+
 const AuthForm = ({ formData, handleChange, handleSubmit, passwordError, loginError, togglePasswordVisibility, showPassword }) => {
+  
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
--- SkyVault/frontend/src/pages/LoginPage.jsx
+++ SkyVault/frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,10 @@
 import React, { useState} from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import AuthForm from '../components/AuthForm';
 import { fetchLoginRequest } from '../redux/reducers/loginSlice';
+import { Loding } from '../components/Loding';
 
 
 export const LoginPage = () => {
@@ -12,8 +13,8 @@ export const LoginPage = () => {
     password: '',
   });
   const dispatch = useDispatch();
-  /*const navigate = useNavigate();*/
   const [showPassword, setShowPassword] = useState(false);
+  const loading = useSelector((state) => state.login.isLoading);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,7 +26,7 @@ export const LoginPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(fetchLoginRequest(formData)); // Теперь просто передаем данные
+    dispatch(fetchLoginRequest(formData)); // Отправляем данные на авторизацию
   };
 
   const togglePasswordVisibility = () => {
@@ -34,21 +35,29 @@ export const LoginPage = () => {
 
   return (
     <div
-      className="container mb-5 mt-5"
-      style={{ paddingTop: '150px', paddingBottom: '50px', minHeight: '100vh' }}
-    >
-      <h1 className="text-center">Вход</h1>
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <AuthForm 
-            formData={formData} 
-            handleChange={handleChange} 
-            handleSubmit={handleSubmit}
-            togglePasswordVisibility={togglePasswordVisibility}
-            showPassword={showPassword}
-          />
+    className="container mb-5 mt-5"
+    style={{ paddingTop: '150px', paddingBottom: '50px', minHeight: '100vh' }}
+  >
+    {loading ? (
+      // Блок загрузки
+      <Loding />
+    ) : (
+      // Основной блок с формой
+      <div>
+        <h1 className="text-center">Вход</h1>
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <AuthForm 
+              formData={formData} 
+              handleChange={handleChange} 
+              handleSubmit={handleSubmit}
+              togglePasswordVisibility={togglePasswordVisibility}
+              showPassword={showPassword}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )}
+  </div>
+);
 };
